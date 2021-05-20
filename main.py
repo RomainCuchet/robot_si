6eecb1e8f6d96612ba0be6439988c384bf7311fe
RomainCuchet <47221720+RomainCuchet@users.noreply.github.com>
2021-05-20 15:52:10 +0200

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
+is_object = False
+def turn_righ():
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+        kitronik_klip_motor.MotorDirection.FORWARD,
+        20)
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+        kitronik_klip_motor.MotorDirection.REVERSE,
+        20)
 def line_follower():
-    if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
-    Kitronik_Clip_Detector.LightSelection.DARK):
-        pass
-    if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
+    if not (is_object):
+        if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
+    Kitronik_Clip_Detector.LightSelection.LIGHT) and Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
     Kitronik_Clip_Detector.LightSelection.LIGHT):
-        pass
-def turn_left(degree):
-    pass
+            move_forward()
+        if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
+    Kitronik_Clip_Detector.LightSelection.DARK) and Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
+    Kitronik_Clip_Detector.LightSelection.LIGHT):
+            turn_left()
+        if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
+    Kitronik_Clip_Detector.LightSelection.LIGHT) and Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
+    Kitronik_Clip_Detector.LightSelection.DARK):
+            turn_righ()
+def turn_left():
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+        kitronik_klip_motor.MotorDirection.REVERSE,
+        20)
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
+        kitronik_klip_motor.MotorDirection.FORWARD,
+        20)
+def obstable_detection():
+    global is_object
+    if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P1,
+    Kitronik_Clip_Detector.LightSelection.OBJCT):
+        is_object = True
+    else:
+        is_object = False
+def move_forward():
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+        kitronik_klip_motor.MotorDirection.FORWARD,
+        20)
+    kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+        kitronik_klip_motor.MotorDirection.FORWARD,
+        20)
 
 def on_forever():
     Kitronik_Clip_Detector.set_sensor_to_detect_objects()
+    obstable_detection()
+    line_follower()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,49 @@
+let is_object = false
+function turn_righ() {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 20)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 20)
+}
+
 function line_follower() {
-    if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Dark)) {
+    if (!is_object) {
+        if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Light) && Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P2, Kitronik_Clip_Detector.LightSelection.Light)) {
+            move_forward()
+        }
         
-    }
-    
-    if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P2, Kitronik_Clip_Detector.LightSelection.Light)) {
+        if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Dark) && Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P2, Kitronik_Clip_Detector.LightSelection.Light)) {
+            turn_left()
+        }
+        
+        if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P0, Kitronik_Clip_Detector.LightSelection.Light) && Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P2, Kitronik_Clip_Detector.LightSelection.Dark)) {
+            turn_righ()
+        }
         
     }
     
 }
 
-function turn_left(degree: any) {
+function turn_left() {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 20)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 20)
+}
+
+function obstable_detection() {
     
+    if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P1, Kitronik_Clip_Detector.LightSelection.Objct)) {
+        is_object = true
+    } else {
+        is_object = false
+    }
+    
+}
+
+function move_forward() {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 20)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 20)
 }
 
 basic.forever(function on_forever() {
     Kitronik_Clip_Detector.setSensorToDetectObjects()
+    obstable_detection()
+    line_follower()
 })
